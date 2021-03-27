e6f238a69429096416dee01fd715a42892b4240b
[Hamid Jebli Ouazzani] <[hamid.jebliouazzani@outlook.com]>
2021-03-27 11:04:49 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Fiji Fiji aloha drinks on me!
+          Fiji Fiji drinks on me!
         </p>
         <a
           className="App-link"
